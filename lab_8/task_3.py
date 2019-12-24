76a4723ecb2b06ff6a974c975f14379a06a7cbb9
igoryartym <igoryartym66@gmail.com>
2019-12-24 13:25:33 +0200

    lab_8

--- lab_8/task_3.py
+++ lab_8/task_3.py
@@ -1,13 +1,13 @@
-n = int(input("Введіть число: "))
-x0 = 0#Рекурсія
-x1 = 9
-if n == 0:
-    print("x= 0")
-elif n == 1:
-    print("x= 9")
-else:
-    for i in range(n - 1):
-        xn = 2 * x1 + 3 * x0
-        x0 = x1
-        x1 = xn
-    print("x(n)= {0}".format(xn))
+def recur(a):
+    if a ==0:
+        return 0
+    elif a == 1:
+        return 9
+    else:
+        return 2*recur(a-1) +3* recur(a-2)
+
+
+n = int(input("count= "))
+a = recur(n)
+print(a)
+
