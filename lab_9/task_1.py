e9c1b0a4b434bcb04b04a7963ba087b642a4bf37
igoryartym <igoryartym66@gmail.com>
2020-01-03 21:52:03 +0200

    lab_9

---
+++ lab_9/task_1.py
@@ -0,0 +1,6 @@
+basa_danix = ['паспортні дані','освіта','спеціальність','посада','оклад']
+basa = {}
+for i in basa_danix:
+    basa[i] = input("{0}: ".format(i))
+print(basa)
+
---
+++ lab_9/task_2.py
@@ -0,0 +1,5 @@
+basa_igru = {'назва гри','вартість','жанр','к_сть рівнів'}
+basa = {}
+for i in basa_igru:
+    basa[i] = input("{0}: ".format(i))
+print(basa)
